54d6fe065365b017355a75565145fa81610b0692
benkigera <benkigera@gmail.com>
2022-11-02 16:02:35 +0300

    Add a basic navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Navbar from "react-bootstrap/Navbar"
+
+function App() {
+  return (
+    <>
+      <Navbar fixed= "top"></Navbar>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>Hello World</div>);
-  
-  
-
-
-
+root.render(<><App /></>);
