fe200d6aca3ab4cc651954c8d5b8f3f3c50e9e26
benkigera <benkigera@gmail.com>
2022-11-02 16:08:26 +0300

    Navbar test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar"
 
 function App() {
   return (
     <>
-      <Navbar fixed= "top"></Navbar>
+      <Navbar fixed= "top">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        </Container>
+      </Navbar>
     </>
   );
 }
